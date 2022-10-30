6115a58fdc95e1d1d203f02817fa00e0de65dad6
ChristopheClaustre <christophe.claustre.31@gmail.com>
2022-10-30 17:46:14 +0100

    [FCT] Authentification and request of all the device of a server + Separation Domoticz Client and Node

--- DomoticzClient.gd
+++ DomoticzClient.gd
@@ -1,14 +1,26 @@
-extends Node
+extends Reference
 class_name DomoticzClient
 
 
-export var host = "127.0.0.1"
-export(int, -1, 65535) var port = -1
-export var use_ssl = false
-export var verify_host = true
+signal connected
+signal polling_error(error)
+signal new_status(status)
+signal body_received(message)
+
+
+var host = "127.0.0.1"
+var port = -1
+var use_ssl = false
+var verify_host = true
+var username_encoded = ""
+var password_encoded = ""
+
+
+var _last_body_received := ""
 
 
 var _client : HTTPClient = null
+var _url = "/json.htm"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,15 +31,75 @@ func _ready():
 			"Please modify parameter of your client and retry connection by calling connect_to_domoticz(true).")
 
 
+func _process(_delta):
+	if not _client:
+		return
+
+	polling()
+
+
+# connect to the signal devices_list_retrieved to have the answer of your request
+func request_devices_list():
+	if not _client:
+		return
+	
+	request_post({"type" : "devices"})
+	pass
+
+
 func connect_to_domoticz(force := false):
 	if _client:
 		if not force:
 			return OK
-		_client.close()
-		_client = null
+		close_connection()
 	
 	_client = HTTPClient.new()
 	var err = _client.connect_to_host(host, port, use_ssl, verify_host)
 	if err != OK:
+		close_connection()
+	
+	return err
+
+
+func close_connection():
+	if _client:
 		_client.close()
 		_client = null
+		_last_status = HTTPClient.STATUS_DISCONNECTED
+
+
+func request_post(body) -> String:
+	var body_str = _client.query_string_from_dict(body)
+	var query_string = _client.query_string_from_dict({"username" : username_encoded, "password" : password_encoded})
+	var headers =  ["Content-Type: application/x-www-form-urlencoded", "Content-Length: " + str(body_str.length())]
+	_client.request(HTTPClient.METHOD_POST, _url + "?" + query_string, headers, body_str)
+	return ""
+
+
+var _last_status = HTTPClient.STATUS_DISCONNECTED
+func polling():
+	if not _client:
+		return
+	
+	var err = _client.poll()
+	if err != OK:
+		emit_signal("polling_error", err)
+		close_connection()
+		return
+
+	# check if status has changed
+	var status = _client.get_status()
+	if status == _last_status:
+		return
+	_last_status = status
+
+	if _last_status == HTTPClient.STATUS_CONNECTED:
+		emit_signal("connected")
+	elif _last_status == HTTPClient.STATUS_BODY:
+		var body = _client.read_response_body_chunk()
+		while _client.has_response() and _client.get_status() == HTTPClient.STATUS_BODY:
+			body.append_array(_client.read_response_body_chunk())
+		_last_body_received = body.get_string_from_utf8()
+		emit_signal("body_received", _last_body_received)
+	
+	emit_signal("new_status", _last_status)
---
+++ DomoticzMainNode.gd
@@ -0,0 +1,83 @@
+extends Node
+class_name DomoticzMainNode
+
+
+signal connected
+signal body_received(message)
+
+
+export var host = "127.0.0.1"
+export(int, -1, 65535) var port = -1
+export var use_ssl = false
+export var verify_host = true
+export var username_encoded = ""
+export var password_encoded = ""
+
+
+onready var _client := DomoticzClient.new()
+var _url = "/json.htm"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_client.connect("polling_error", self, "_on_client_polling_error")
+	_client.connect("new_status", self, "_on_client_new_status")
+	
+	_client.host = host
+	_client.port = port
+	_client.use_ssl = use_ssl
+	_client.verify_host = verify_host
+	_client.username_encoded = username_encoded
+	_client.password_encoded = password_encoded
+	
+	var err = _client.connect_to_domoticz()
+	if err != OK:
+		push_warning("Problem with the configuration of DomoticzClient." +
+			"Please modify parameter of your client and retry connection by calling connect_to_domoticz(true).")
+
+
+func _process(_delta):
+	_client.polling()
+
+
+func request_devices_list():
+	_client.request_devices_list()
+
+
+func _on_client_polling_error(error):
+	print_log("Polling failed. Connection must have been closed by server.", true)
+
+
+func _on_client_new_status(status):
+	if status == HTTPClient.STATUS_DISCONNECTED:
+		print_log("Disconnected!")
+	elif status == HTTPClient.STATUS_RESOLVING:
+		print_log("Resolving...")
+	elif status == HTTPClient.STATUS_CANT_RESOLVE:
+		print_log("Can't resolve hostname. Check the given hostname.", true)
+	elif status == HTTPClient.STATUS_CONNECTING:
+		print_log("Connecting...")
+	elif status == HTTPClient.STATUS_CANT_CONNECT:
+		print_log("Can't connect to server. Check the settings of your client and your server.", true)
+	elif status == HTTPClient.STATUS_CONNECTED:
+		print_log("Connected!")
+	elif status == HTTPClient.STATUS_REQUESTING:
+		print_log("Requesting...")
+	elif status == HTTPClient.STATUS_BODY:
+		print_log("New body received!")
+	elif status == HTTPClient.STATUS_CONNECTION_ERROR:
+		print_log("HTTP connection error. Check the settings of your client and your server. Check internet connection.", true)
+	elif status == HTTPClient.STATUS_SSL_HANDSHAKE_ERROR:
+		print_log("SSL handshake error. Is your server configured to accepts ssl connection (https) ?", true)
+	else:
+		print_log("Unknown status", true)
+
+
+
+
+func print_log(message : String, warning := false):
+	var _message = "DOMOTICZPLUGIN: " + message
+	if warning:
+		push_warning(_message)
+	else:
+		print(_message)
