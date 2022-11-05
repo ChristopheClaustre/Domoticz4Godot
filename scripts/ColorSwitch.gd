ee01612ff97a8720dc7b899583e719432e1d8b2e
ChristopheClaustre <christophe.claustre.31@gmail.com>
2022-11-05 01:09:48 +0100

    [FCT] Store status of ColorSwitch (and update it in case of switch_on/off())

--- scripts/ColorSwitch.gd
+++ scripts/ColorSwitch.gd
@@ -9,26 +9,30 @@ func get_class():
 
 
 var color : DzColor
+var status : String = ""
 
 
 func _internal_init(device_info, dz_main_node):
 	._internal_init(device_info, dz_main_node)
+	status = device_info["Status"]
 	if device_info.has("Color") and not device_info["Color"].empty():
 		color = DzColor.new(parse_json(device_info["Color"]))
 	else:
 		color = DzColor.new()
-	pass
 
 
 func switch_on():
+	status = "Off"
 	dzMainNode.request_switchlight(idx, "On")
 
 
 func switch_off():
+	status = "Off"
 	dzMainNode.request_switchlight(idx, "Off")
 
 
 func switch_toggle():
+	status = "Off" if status == "On" else "On"
 	dzMainNode.request_switchlight(idx, "Toggle")
 
 
