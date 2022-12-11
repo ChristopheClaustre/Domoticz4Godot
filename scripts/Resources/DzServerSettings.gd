c11098d0d62bb6bbb8a46facf3ecec68ebdfcd90
ChristopheClaustre <christophe.claustre.31@gmail.com>
2022-12-11 01:29:05 +0100

    [QA] DzServerSettings : add "export" keywords on each attributes, to be able to duplicate and serialize them

--- scripts/Resources/DzServerSettings.gd
+++ scripts/Resources/DzServerSettings.gd
@@ -3,12 +3,12 @@ class_name DzServerSettings
 
 
 # server settings
-var host := ""
-var port := 8080
-var use_ssl := true
-var verify_host := true
-var username_encoded := ""
-var password_encoded := ""
+export var host := ""
+export var port := 8080
+export var use_ssl := true
+export var verify_host := true
+export var username_encoded := ""
+export var password_encoded := ""
 
 func _set(property, value):
 	emit_changed()
